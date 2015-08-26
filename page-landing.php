5d68f5ce4d4200f66b31444bd39d372b947db05e
quinterpriest <quinterpriest@gmail.com>
2015-08-26 14:40:47 -0500

    a

--- page-landing.php
+++ page-landing.php
@@ -1,8 +1,9 @@
+<?php
 /*
 
 Template Name: Page Landing
 
-*/
+*/?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -16,14 +17,14 @@ Template Name: Page Landing
 
     <!-- Bootstrap core CSS -->
     <!-- Latest compiled and minified CSS -->
-    <link href="<?php echo get_template_directory_uri();?>/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri();?>/../church-event-child/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="<?php echo get_template_directory_uri();?>/css/cover.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri();?>/../church-event-child/css/cover.css" rel="stylesheet">
 
     <!-- Custom css and fonts -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,300' rel='stylesheet' type='text/css'>
-    <link href="<?php echo get_template_directory_uri();?>/css/font-awesome.min.css" rel="stylesheet" >
+    <link href="<?php echo get_template_directory_uri();?>/../church-event-child/css/font-awesome.min.css" rel="stylesheet" >
 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -41,9 +42,9 @@ Template Name: Page Landing
 <!-- Header -->
 
 
-<section id="top" class="site-wrapper">
+<section id="top" class="site-wrapper" style="background-image: url(http://sergioromero.cl/vesol/wp-content/uploads/ss-2-bg.jpg);">
     <!-- Video from YouTube. Have Questions? How To: https://github.com/pupunzi/jquery.mb.YTPlayer/wiki -->
-    <a id="bgndVideo" class="player" data-property="{videoURL:'http://youtu.be/rlYVhDKCbqY', containment:'.site-wrapper', autoPlay:true, loop:true, mute:true, startAt:0, stopAt: 50, quality:'small', opacity:1, showControls: false, showYTLogo:false, vol:25}"></a>
+    <a id="bgndVideo" class="player" data-property="{videoURL:'http://youtu.be/rlYVhDKCbqY', containment:'.site-wrapper', autoPlay:true, loop:true, mute:true, startAt:0, stopAt: 50, realfullscreen:true, quality:'small', opacity:1, showControls: false, showYTLogo:false, vol:25}"></a>
     <div class="overlay"></div>
     <div class="site-wrapper-inner">
         <div class="masthead clearfix wow fadeIn" data-wow-delay=".9s">
@@ -52,7 +53,7 @@ Template Name: Page Landing
                     <a href="tel:1234567890">
                         <i class="icon ion-android-call"></i> Saltar Intro
                         <small> 
-                            <img width="90px" src="<?php echo get_template_directory_uri();?>/images/logo.png" alt="">
+                            <img width="90px" src="<?php echo get_template_directory_uri();?>/../church-event-child/images/logo.png" alt="">
                         </small>
                     </a>
                 </h3>
@@ -85,19 +86,19 @@ Template Name: Page Landing
 
 
 <!-- jQuery -->
-<script src="<?php echo get_template_directory_uri();?>/js/jquery.min.js"></script>
+<script src="<?php echo get_template_directory_uri();?>/../church-event-child/js/jquery.min.js"></script>
 
 <!-- Bootstrap Core JavaScript -->
-<script src="<?php echo get_template_directory_uri();?>/js/bootstrap.min.js"></script>
+<script src="<?php echo get_template_directory_uri();?>/../church-event-child/js/bootstrap.min.js"></script>
 
 <!-- Plugins for video background -->
-<script src="<?php echo get_template_directory_uri();?>/js/device.min.js"></script>
-<script src="<?php echo get_template_directory_uri();?>/js/jquery.mb.YTPlayer.js"></script>
+<script src="<?php echo get_template_directory_uri();?>/../church-event-child/js/device.min.js"></script>
+<script src="<?php echo get_template_directory_uri();?>/../church-event-child/js/jquery.mb.YTPlayer.js"></script>
 
 <!-- Plugins -->
-<script src="<?php echo get_template_directory_uri();?>/js/wow.min.js"></script>
-<script src="<?php echo get_template_directory_uri();?>/js/jquery.countdown.min.js"></script>
-<script src="<?php echo get_template_directory_uri();?>/js/smoothscroll.js"></script>
+<script src="<?php echo get_template_directory_uri();?>/../church-event-child/js/wow.min.js"></script>
+<script src="<?php echo get_template_directory_uri();?>/../church-event-child/js/jquery.countdown.min.js"></script>
+<script src="<?php echo get_template_directory_uri();?>/../church-event-child/js/smoothscroll.js"></script>
 <script>
     
     var fecha_contador="2015/10/31";
@@ -105,7 +106,7 @@ Template Name: Page Landing
 </script>
 
 <!-- Custom JavaScript -->
-<script src="<?php echo get_template_directory_uri();?>/js/cover.js"></script>
+<script src="<?php echo get_template_directory_uri();?>/../church-event-child/js/cover.js"></script>
 
 </body>
 </html>
