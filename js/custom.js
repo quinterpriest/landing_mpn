0c2de321f654cdb9ab404b923b9d29f46222b1a1
quinterpriest <quinterpriest@gmail.com>
2015-08-26 12:46:55 -0500

    nuevo

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function () {
+
+    //jQuery(".player").mb_YTPlayer();
+
+    var options = { videoId: 'e4Is32W-ppk', start: 3 };
+	jQuery('#wrapper').tubular(options);
+
+});
